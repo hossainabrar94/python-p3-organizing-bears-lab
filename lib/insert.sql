194deff429d051ab14cb2a879ba9fc4e697f3021
Abrar <hossainabrar@aol.com>
2024-06-02 18:35:49 -0400

    complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1, 'Mr. Chocolate', 4, 'M', 'brown', 'happy', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2, 'Rowdy', 7, 'M', 'red', 'mad', 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3, 'Tabitha', 4, 'F', 'brown', 'happy', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (4, 'Sergeant Brown', 4, 'M', 'brown', 'happy', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5, 'Melissa', 4, 'F', 'brown', 'happy', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6, 'Grinch', 6, 'M', 'white', 'happy', 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7, 'Wendy', 4, 'F', 'brown', 'happy', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (8, null, 4, 'F', 'brown', 'happy', 1);
